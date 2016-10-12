7099321107a2ce36d1316f0a2dd8d17d7f59ce9b
cbitt <csbango@yahoo.com>
2016-10-12 03:30:00 +0100

    enum class created for Builder

--- src/Builder.java
+++ src/Builder.java
@@ -2,9 +2,11 @@
  * Created by BangoCs on 10/12/2016.
  */
 public enum Builder {
+
     FENDER, MARTIN, GIBSON, COLLINGS, OLSON, RYAN, PRS, ANY;
 
     public String toString() {
+
         switch(this) {
             case FENDER:   return "Fender";
             case MARTIN:   return "Martin";
