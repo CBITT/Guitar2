de7cd2ed39b14a40e7cd8f38ce56283b75f8ebc4
cbitt <csbango@yahoo.com>
2016-10-12 03:27:35 +0100

    enum class created for Type

--- src/Type.java
+++ src/Type.java
@@ -7,7 +7,6 @@ public enum Type {
     public String toString() {
 
         switch(this) {
-
             case ACOUSTIC: return "acoustic";
             case ELECTRIC: return "electric";
             default:       return "unspecified";
