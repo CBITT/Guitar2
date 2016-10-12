a5900c317fcdfe18a9df02003123599b32180911
cbitt <csbango@yahoo.com>
2016-10-12 02:31:38 +0100

    problem with Builder type migration

---
+++ test/GuitarTest.java
@@ -0,0 +1,51 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by BangoCs on 10/12/2016.
+ */
+public class GuitarTest {
+    @Test
+    public void getSerialNumber() throws Exception {
+
+    }
+
+    @Test
+    public void getPrice() throws Exception {
+
+    }
+
+    @Test
+    public void setPrice() throws Exception {
+
+    }
+
+
+    @Test
+    public void getBuilder() throws Exception {
+
+    }
+
+    @Test
+    public void getModel() throws Exception {
+
+    }
+
+    @Test
+    public void getType() throws Exception {
+
+    }
+
+    @Test
+    public void getBackWood() throws Exception {
+
+    }
+
+    @Test
+    public void getTopWood() throws Exception {
+
+    }
+
+
+}
